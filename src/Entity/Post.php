eec5fb6c7dd6bf3f82371b515cb20acf1e7f1af9
erdaserhan <erhan209@gmail.com>
2024-09-11 15:53:41 +0200

    post.php setter et getters updated

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
@@ -13,8 +14,38 @@ class Post
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 160)]
+    private ?string $postTitle = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $postText = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPostTitle(): ?string
+    {
+        return $this->postTitle;
+    }
+
+    public function setPostTitle(string $postTitle): static
+    {
+        $this->postTitle = $postTitle;
+
+        return $this;
+    }
+
+    public function getPostText(): ?string
+    {
+        return $this->postText;
+    }
+
+    public function setPostText(string $postText): static
+    {
+        $this->postText = $postText;
+
+        return $this;
     }
 }
