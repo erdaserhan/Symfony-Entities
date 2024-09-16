798b1a6a55916c63747dbb1671d4d6e088e8927a
erdaserhan <erhan209@gmail.com>
2024-09-16 15:13:30 +0200

    crud section update and bootstrap adapted

--- src/Form/SectionType.php
+++ src/Form/SectionType.php
@@ -2,9 +2,8 @@
 
 namespace App\Form;
 
-use App\Entity\Post;
+
 use App\Entity\Section;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,11 +15,7 @@ class SectionType extends AbstractType
         $builder
             ->add('sectionTitle')
             ->add('sectionDescription')
-            ->add('posts', EntityType::class, [
-                'class' => Post::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
+
         ;
     }
 
