2d0c4899b7f2239e21a20272840cf0af2abd6bdd
erdaserhan <erhan209@gmail.com>
2024-09-24 14:29:08 +0200

    links updated meta

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+import './bootstrap.bundle.min.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,6 +7,6 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-import './vendor/bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
