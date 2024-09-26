4c711f5017a6d2d683c78970e1825809b79f38b0
erdaserhan <erhan209@gmail.com>
2024-09-26 11:22:49 +0200

    Section Controller menu ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+# appel du gestionnaire de section
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,20 +11,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(SectionRepository $sections): Response
     {
         return $this->render('main/index.html.twig', [
             'title' => 'Homepage',
             'homepage_text' => "Nous sommes le ".date('d/m/Y \à H:i'),
+            # on met dans une variable pour twig toutes les sections récuperées
+            'sections' => $sections->findAll()
         ]);
     }
 
     #[Route('/about', name: 'about_me')]
-    public function aboutMe(): Response
+    public function aboutMe(SectionRepository $sections): Response
     {
         return $this->render('main/about.html.twig', [
             'title' => 'About me Baby',
             'homepage_text' => "Et C'est moi !",
+            'sections' => $sections->findAll()
         ]);
     }
 }
